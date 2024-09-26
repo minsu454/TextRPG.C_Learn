e38c9786b23512248e256b7e89dcb0cd73a8c677
minsu454 <kminsu454@gmail.com>
2024-09-26 13:04:10 +0900

    [Add] 폴더추가

--- TextRPG/TextRPG/Program.cs
+++ TextRPG/TextRPG/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace TextRPG
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello, World!");
+    //    }
+    //}
 }
