6ccac9100b31b6a247b27333e0341f923f3067e9
Unity-js <essaround@gmail.com>
2024-09-27 15:13:59 +0900

    [Fix] Stageinfo

--- TextRPG/TextRPG/Stageinfo/Stageinfo.cs
+++ TextRPG/TextRPG/Stageinfo/Stageinfo.cs
@@ -1,6 +1,6 @@
 ﻿namespace TextRPG
 {
-    public class StageInfo : BaseScene
+    public class StageInfo
     {
         // mob 쪽 코드 작성되면 옮길 곳 , 지금은 출력을 위한 예시
 
@@ -11,29 +11,32 @@
         public static int[] mobhp = new int[] { 1, 2, 3 }; // 몹 체력
 
         public static int stagecount = 1; // 스테이지
-
-        public static int[] mobType = new int[3]; 
-
-        public static void MobCount() // 몹 종류, 수 랜덤 함수
+        public int[] MobCount() // 몹 랜덤 함수
         {
             Random random = new Random();
 
             int mobcount = random.Next(1, 4); // 등장하는 몹의 수
 
-            for(int i = 0; i < mobcount; i++)
+            return MobReturn(mobcount);
+        }
+
+        private int[] MobReturn(int mobCount)
+        {
+            int[] mobArray = new int[mobCount];
+
+            for (int i = 0; i < mobCount; i++)
 
             {
+                Random random = new Random();
+
                 int mobtype = random.Next(0, 3); // 등장하는 몹의 종류
 
-                Console.WriteLine("Lv:" + moblevel[mobtype] + mobname[mobtype] + "Hp:" + mobhp[mobtype]); // 출력
-
-                mobType[i] = mobtype;
+                mobArray[i] = mobtype;
             }
 
+            return mobArray;
         }
-        public override void Load()
-        {
-            GameManager.Scene.CloseScene();
-        }
+
     }
+
 }
