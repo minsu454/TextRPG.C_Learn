1d517de29692d7b82faac64e35860e7638d2732c
Unity-js <essaround@gmail.com>
2024-09-27 20:29:43 +0900

    [Update] StageInfo

--- TextRPG/TextRPG/Stageinfo/Stageinfo.cs
+++ TextRPG/TextRPG/Stageinfo/Stageinfo.cs
@@ -2,16 +2,11 @@
 {
     public class StageInfo
     {
-        // mob 쪽 코드 작성되면 옮길 곳 , 지금은 출력을 위한 예시
+        public int stagecount = 1; // 스테이지, 전투 쪽 코드에서 승리시 > 이 값 ++
 
-        public static string[] mobname = new String[] { "name1", "name2", "name3" }; // 몹 이름
-        public static int[] moblevel = new int[] { 1, 2, 3 }; // 몹 레벨
-        public static int[] mobatk = new int[] { 1, 2, 3 }; // 몹 공격력
-        public static int[] mobdef = new int[] { 1, 2, 3 }; // 몹 방어력
-        public static int[] mobhp = new int[] { 1, 2, 3 }; // 몹 체력
+        public List<Monster> monsters = new List<Monster>();
 
-        public static int stagecount = 1; // 스테이지
-        public int[] MobCount() // 몹 랜덤 함수
+        public int[] MobCount() // 몹 랜덤
         {
             Random random = new Random();
 
@@ -20,7 +15,32 @@
             return MobReturn(mobcount);
         }
 
-        private int[] MobReturn(int mobCount)
+        public void Init()
+        {
+            int[] arr = MobCount();
+
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++) // arr (return 받아온 배열)로 몹 종류 정하기
+            {
+                if (arr[i] == 0)
+                {   
+                    int moblevel = random.Next(1, 5); // 레벨 1~4 랜덤
+
+                    monsters.Add(new Ork(moblevel, "Ork"));
+                }
+                else
+                {   
+                    int moblevel = random.Next(1, 5); // 레벨 1~4 랜덤
+
+                    monsters.Add(new Goblin(moblevel, "Goblin"));
+                }
+
+
+            }
+        }
+
+        public int[] MobReturn(int mobCount)
         {
             int[] mobArray = new int[mobCount];
 
@@ -29,7 +49,7 @@
             {
                 Random random = new Random();
 
-                int mobtype = random.Next(0, 3); // 등장하는 몹의 종류
+                int mobtype = random.Next(0, 2); // 등장하는 몹의 종류
 
                 mobArray[i] = mobtype;
             }
