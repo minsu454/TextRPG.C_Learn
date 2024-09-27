42d54b2243837c612fedcfbdfabb302bfe045307
김홍섭 <qufrj7@naver.com>
2024-09-27 15:06:15 +0900

    [Fix]Monster
    class 구분

--- TextRPG/TextRPG/Monster/Monster.cs
+++ TextRPG/TextRPG/Monster/Monster.cs
@@ -26,6 +26,7 @@
         public int Defence { get; set; }
         public bool IsDead => Health <= 0;
         
+        // 몬스터가 데미지를 받았을 경우에 나오는 결과
 
         public void TakeDamage(int damage)
         {
@@ -38,82 +39,8 @@
                 Health -= damage;
                 Console.WriteLine($"{Name}이(가) {damage}의 데미지를 받았습니다. 남은 체력: {Health}");
             }
-        }
-
-        //고블린 클래스
-
-        public class Goblin : Monster
-        {
-         public Goblin(int level, string name, int health, int attack, int defence)
-         {
-            Level = level;
-            Name = name;
-            Health = health;
-            Attack = attack;
-            Defence = defence;
-         }
-            int[] level = { 1, 2, 3, 4 };
-            int[] health = { 50, 70, 100, 150 };
-            int[] attack = { 10, 20, 30, 40 };
-            int[] defence = { 5, 10, 15, 20 };
-
-           public (int Health, int Attack, int Defence) GetStatsByLevel(int level)
-           {
-            switch (level)
-            {
-                case 1:
-                    return (50, 10, 5);
-                case 2:
-                    return (70, 20, 10);
-                case 3:
-                    return (100, 30, 15);
-                case 4:
-                    return (150, 40, 20);
-                default:
-                    return (0, 0, 0); // 유효하지 않은 레벨의 경우 기본값
-            }
-           }        
-         
-            
-        }
-
-        //오크 클래스
-
-        public class Ork : Monster
-        {
-            public Ork(int level, string name, int health, int attack, int defence)
-            {
-                Level = level;
-                Name = name;
-                Health = health;
-                Attack = attack;
-                Defence = defence;
-            }
-            int[] level = { 1, 2, 3, 4 };
-            int[] health = { 60, 80, 110, 160 };
-            int[] attack = { 15, 25, 35, 45 };
-            int[] defence = { 10, 15, 20, 25 };
-
-            public (int Health, int Attack, int Defence) GetStatsByLevel(int level)
-            {
-                switch (level)
-                {
-                    case 1:
-                        return (60, 15, 10);
-                    case 2:
-                        return (80, 25, 15);
-                    case 3:
-                        return (110, 35, 20);
-                    case 4:
-                        return (160, 45, 25);
-                    default:
-                        return (0, 0, 0); // 유효하지 않은 레벨의 경우 기본값
-                }
-            }
-
-            
-        }
-
+        }     
+             
 
     }
 }
