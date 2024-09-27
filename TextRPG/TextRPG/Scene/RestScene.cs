bdc249fcc1a95451da21756bde574e59fa456882
김홍섭 <qufrj7@naver.com>
2024-09-27 20:24:15 +0900

    [fix]restscence

--- TextRPG/TextRPG/Scene/RestScene.cs
+++ TextRPG/TextRPG/Scene/RestScene.cs
@@ -1,47 +1,37 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using System.Text;
-
-namespace TextRPG
+﻿namespace TextRPG
 {
-    public class RestScene : BaseScene
+    public class RestScene : BaseScene                               
+    {
+        //1. 휴식 씬에 들어오면 "휴식하시겠습니까?" 문구가 나오고 "1. 예" 문구와 "2. 아니오." 문구가 나온다.
+        //2. "예"(숫자키 1)를 선택 시 체력이 충전되고 "체력이 충전되었습니다." 문구가 나온다. 그 후 로비로 돌아온다.
+        //3. "아니오"(숫자키 2)를 선택 시 바로 로비로 돌아온다.
+        //4. 숫자키 1, 2 이외의 다른 키를 누르면 반응이 없다.
 
-
-            //1. 휴식 씬에 들어오면 "휴식하시겠습니까?" 문구가 나오고 "1. 예" 문구와 "2. 아니오." 문구가 나온다.
-            //2. "예"(숫자키 1)를 선택 시 체력이 충전되고 "체력이 충전되었습니다." 문구가 나온다. 그 후 로비로 돌아온다.
-            //3. "아니오"(숫자키 2)를 선택 시 바로 로비로 돌아온다.
-            //4. 숫자키 1, 2 이외의 다른 키를 누르면 반응이 없다.
-
-            //필요한 기능
-            //현재 체력은 최신화 되어야 한다?
-            //던전 입장 후 중간에 로비로 돌아오고 그 다음에 휴식 공간 입장 가능?
-            //전투 후 몬스터 혹은 기타 상황에 의해 깎인 체력 데이터?
-
-            
-    {       
+        //필요한 기능
+        //현재 체력은 최신화 되어야 한다?
+        //던전 입장 후 중간에 로비로 돌아오고 그 다음에 휴식 공간 입장 가능?
+        //전투 후 몬스터 혹은 기타 상황에 의해 깎인 체력 데이터?
 
         public override void Load()
         {
             {   
-                Console.WriteLine("현재 체력: ");  //플레이어의 현재 체력
+
+                Console.WriteLine($"현재 체력: {GameManager.player.playerCurHealth}");  //플레이어의 현재 체력
                 Console.WriteLine("휴식하시겠습니까?");
 
                 ChooseChoice();
-                
-                GameManager.player.playerCurHealth = GameManager.player.playerMaxHealth;
+                                
                 GameManager.Scene.CloseScene();                
             }                       
         }
         public void ChooseChoice()
         {
-            bool validSelection = false;
-
-            while (!validSelection) 
-            {
+                        
                 Console.WriteLine("예.");
                 Console.WriteLine("아니오.");
 
                 // ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                int input =  Input.InputKey(2);
+                int input = Input.InputKey(2);
 
                 //if (keyInfo.Key == ConsoleKey.D1) //딜레이를 걸 것인지 "로비 화면으로 넘어가시겠습니까" 선택 UI를 만들 것인지
                 //{
@@ -62,36 +52,14 @@ namespace TextRPG
                 //}
 
                 if (input == 1)
-                { 
-                    Console.WriteLine("예");
+                {                     
+                    GameManager.player.playerCurHealth = GameManager.player.playerMaxHealth;
                 }
-                else
-                {
-                    Console.WriteLine("아니오"); 
-                }
+                                
+                    
+    }
 
-                //switch (keyInfo.Key)
-                //{
-                //    case ConsoleKey.D1:
-                //        validSelection = true;
-                //        break;
-                //    case ConsoleKey.D2:
-                //        validSelection = true;
-                //        break;
-                //    default:
-
-                //        break;
-                //}
-            }
-        }
-
-        public void ShowHealth(int playerCurHealth)
-        {
-
-        }
-        #region PrintFormat
-
-        #endregion
+        
 
     }
 
