0049ea5c4fc0310e6b3f19f7f5ff007a0a8243f5
minsu454 <kminsu454@gmail.com>
2024-10-02 19:18:00 +0900

    [Fix] LobbyScene

--- TextRPG/TextRPG/Scene/LobbyScene.cs
+++ TextRPG/TextRPG/Scene/LobbyScene.cs
@@ -11,18 +11,12 @@
 
         public override void Load()
         {
-            Console.Write(lobbyFormat);
+            Print.ColorPrintScreen(ConsoleColor.Green, $"이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
             int input = Input.Selection(1, "상태 보기", "인벤토리", "상점", "퀘스트", "던전입장", "휴식하기", "저장하기");
 
             GameManager.Scene.OpenScene((SceneType)(input + shiftCount));
         }
 
-        #region PrintFormat
-        private string lobbyFormat =
-@$"이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.
-
-";
-        #endregion
     }
 }
 
